7846ba7be63025e8f657f96f5752f7bb833acdf1
Ruiyan <2027liu.ruiyan@fcboe.org>
2024-01-22 15:42:14 -0500

    message

---
+++ c/basics.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+
+	printf("This is my c basics program");
+	int num;
+	printf("Input number:");
+	scanf("%i", &num);
+	printf("You entered %i/n", num);
+	return 0;
+}
+
